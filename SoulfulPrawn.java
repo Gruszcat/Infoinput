619929c687167e3bc2fcf6d4bffe656b4a2ffb69
Gruszcat <66838959+Gruszcat@users.noreply.github.com>
2020-06-18 12:21:54 -0500

    Add files via upload

---
+++ SoulfulPrawn.java
@@ -0,0 +1,69 @@
+
+
+
+import java.util.*; 
+
+
+
+public class SoulfulPrawn {
+
+  
+  public static void main(String[] args) {
+
+    
+    Scanner user_input = new Scanner(System.in);
+
+   
+    System.out.println("SoulfulPrawn\n\n");
+
+
+
+    System.out.println("What is your full first name?:");
+
+    String something1 = user_input.nextLine();
+
+    System.out.println("\n");
+
+
+
+    System.out.println("What is your middle name?");
+
+    String something2 = user_input.nextLine();
+
+    System.out.println("\n");
+
+
+
+    System.out.println("What is your last name?");
+
+    String something3 = user_input.nextLine();
+
+    System.out.println("\n");
+
+
+
+    System.out.println("What is your birth month?");
+
+    String something4 = user_input.nextLine();
+
+    System.out.println("\n");
+
+
+
+    System.out.println("What is your birth year?");
+
+    String something5 = user_input.nextLine();
+
+    System.out.println("\n\n");
+
+
+  
+
+    System.out.println("Full Name: " + something1 + " " + something2 + " " + something3);
+
+    System.out.println("Born: " + something4 + " " + something5);
+
+
+
+  }
+}
